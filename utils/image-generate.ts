08a074e5b9d689e16c9d257a2586cd7d76afa4fb
abderrahim101 <abderrahimaneddam1@gmail.com>
2023-05-14 21:02:33 +0100

    fix a bug

--- utils/image-generate.ts
+++ utils/image-generate.ts
@@ -1,8 +1,6 @@
 import BLOG from "../BLOG.config";
 
-const API_CLIENTID = BLOG.unsplashApiKey;
-const API_URL = `https://api.unsplash.com/photos/random?client_id=${API_CLIENTID}&query=nature&count=1`;
-
+const API_URL = BLOG.RandomImagesEndPoint;
 let GenerateImage = async () => {
   let images = await fetch(API_URL).then((response) => response.json());
   // .then((result) => result.results);
